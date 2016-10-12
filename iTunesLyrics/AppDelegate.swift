4affce9a97c90f051d758c8b23d1f4c0cbbfcae1
Jimmy Sinn <jimmyltsinn@gmail.com>
2016-10-12 17:56:02 +0800

    Initial Commit

---
+++ iTunesLyrics/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iTunesLyrics
+//
+//  Created by Jimmy Sinn on 12/10/2016.
+//  Copyright © 2016 Jimmy Sinn. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ iTunesLyrics/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iTunesLyrics
+//
+//  Created by Jimmy Sinn on 12/10/2016.
+//  Copyright © 2016 Jimmy Sinn. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
